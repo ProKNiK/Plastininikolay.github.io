08a577c146ec9d9d9ae6c00fd4e1fa650060ca3f
Nikolay <plastininikolay@yandex.ru>
2020-10-14 01:21:29 +0800

    small corrections, smooth skrolling

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,17 @@ counters.forEach((item, i) => {
         lines[i].style.width = '100%'
     }
 });
+const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
